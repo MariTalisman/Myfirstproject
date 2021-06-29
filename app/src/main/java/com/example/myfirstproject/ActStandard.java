f525491792ea2e435d8849f678c421def272f5c7
Ivan Larin <larin.i.v@sberbank.ru>
2021-06-29 20:59:33 +0300

    Лекция 2, работа с Activity (Standard, SingleTask)

---
+++ app/src/main/java/com/example/myfirstproject/ActStandard.java
@@ -0,0 +1,29 @@
+package com.example.myfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActStandard extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text);
+        textView.setText("ActivityStandard");
+
+        findViewById(R.id.but1).setOnClickListener(v -> startStandard());
+        findViewById(R.id.but2).setOnClickListener(v -> startSingleTask());
+    }
+
+    private void startStandard() {
+        startActivity(new Intent(this, ActStandard.class));
+    }
+
+    private void startSingleTask() {
+        startActivity(new Intent(this, ActTask.class));
+    }
+}
